459f15f81e5b1a1cc48cd8f72f2552475e7050df
zeroIndex0 <sagtogi@gmail.com>
2020-10-18 12:56:36 -0400

    added session variables for update functionality

--- index.php
+++ index.php
@@ -15,30 +15,9 @@
   <?php include_once "api/post/read.php"; ?>
   <?php $show_posts = false; ?>
 
-  <!-- <?php if (isset($_POST["show_posts"])) : ?>
-    <?php $show_posts = true; ?>
-    <table class="highlight">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Body</th>
-          <th>Author</th>
-        </tr>
-      </thead>
-      <?php for ($i = 0; $i < sizeof($data_array["data"]); $i++) : ?>
-        <tr>
-          <td class="left"><?php echo $data_array["data"][$i]["title"] ?></td>
-          <td><?php echo $data_array["data"][$i]["body"] ?></td>
-          <td class="right"><?php echo $data_array["data"][$i]["author"] ?></td>
-        </tr>
-      <?php endfor; ?>
-    </table>
-  <?php endif; ?> -->
-
-
-  <!-- Delete file used on button push with data-update-id nad data-delete-id -->
+  <!-- Delete file used on button push with data-update-id and data-delete-id -->
   <?php require_once "api/post/delete.php"; ?>
-  <?php require_once "api/post/update.php"; ?>
+  <?php require_once "api/post/read_one.php"; ?>
 
   <?php if (isset($_POST["show_posts"])) : ?>
     <?php $show_posts = true; ?>
@@ -51,10 +30,10 @@
           <p class="is-size-5-mobile has-text-dark"><?php echo $data_array["data"][$i]["author"] ?></p>
           <br>
           <div class="field is-grouped">
-            <form method="POST" action="api/post/update.php">
+            <form method="POST" action="api/post/read_one.php">
               <!-- This needs to be a read_one where the single read will be stored into a variable to be used for update -->
               <!-- The value can be accessed from the name via $_POST which is how we get the id to update or delete -->
-              <button name="update_button_request" value="<?php echo $data_array["data"][$i]["id"]; ?>" class="button has-background-info has-text-white" type="submit">Update</button>
+              <button name="edit_button_request" value="<?php echo $data_array["data"][$i]["id"]; ?>" class="button has-background-info has-text-white" type="submit">Edit</button>
             </form>
             <form method="POST" action="api/post/delete.php">
               <!-- The value can be accessed from the name via $_POST which is how we get the id to update or delete -->
@@ -98,38 +77,85 @@
 
 
   <?php require_once "api/post/create.php"; ?>
+  <?php require_once "api/post/update.php"; ?>
+  <?php
+  if (!isset($_SESSION["read_one_entry_flag"])) {
+    $_SESSION["read_one_entry_flag"] = false;
+  } else {
+    echo "<h1>Read One Entry Flag: " . $_SESSION["read_one_entry_flag"] . "</h1>";
+    if (isset($_SESSION["read_one_entry"])) {
+      echo "Author in index: " . $_SESSION["read_one_entry"]["author"];
+    } else {
+      echo "No Post Selected";
+    }
+  }
+  ?>
 
-  <div class="column is-three-quarters">
-    <form action="api/post/create.php" method="POST">
-      <div class="field">
-        <label class="label">Title:</label>
-        <input id="form_title" class="input is-primary" type="text" name="form_title" placeholder="Title of Post" required>
-      </div>
-      <div class="field">
-        <label class="label">Body:</label>
-        <textarea id="form_body" class="textarea" placeholder="e.g. Today I..." name="form_body" required></textarea>
-      </div>
-      <div class="field">
-        <label class="label">Author:</label>
-        <div class="control has-icons-left">
-          <input id="form_author" class="input is-primary" type="text" name="form_author" placeholder="Author of Post" required>
-          <span class="icon is-small is-left">
-            <i class="material-icons prefix">account_circle</i>
-          </span>
+  <?php if ($_SESSION["read_one_entry_flag"] === false) : ?>
+    <div class="column is-three-quarters">
+      <form action="api/post/create.php" method="POST">
+        <div class="field">
+          <label class="label">Title:</label>
+          <input id="form_title" class="input is-primary" type="text" name="form_title" placeholder="Title of Post" required>
         </div>
-      </div>
-      <button class="button is-primary" type="submit" name="submit_create_request">
-        <span>Submit</span>
-        <span class="icon">
-          <i class="material-icons prefix">send</i>
-        </span>
-      </button>
-    </form>
-  </div>
+        <div class="field">
+          <label class="label">Body:</label>
+          <textarea id="form_body" class="textarea" placeholder="e.g. Today I..." name="form_body" required></textarea>
+        </div>
+        <div class="field">
+          <label class="label">Author:</label>
+          <div class="control has-icons-left">
+            <input id="form_author" class="input is-primary" type="text" name="form_author" placeholder="Author of Post" required>
+            <span class="icon is-small is-left">
+              <i class="material-icons prefix">account_circle</i>
+            </span>
+          </div>
+        </div>
+        <button class="button is-primary" type="submit" name="submit_create_request">
+          <span>Submit</span>
+          <span class="icon">
+            <i class="material-icons prefix">send</i>
+          </span>
+        </button>
+      <?php else : ?>
+        <div class="column is-three-quarters">
+          <form action="api/post/update.php" method="POST">
+            <div class="field">
+              <label class="label">Title:</label>
+              <input id="form_title" class="input is-primary" type="text" name="form_title" value="<?php echo $_SESSION["read_one_entry"]["title"]; ?>" placeholder="Title of Post" required>
+            </div>
+            <div class="field">
+              <label class="label">Body:</label>
+              <textarea id="form_body" class="textarea" placeholder="e.g. Today I..." name="form_body" required><?php echo $_SESSION["read_one_entry"]["body"]; ?></textarea>
+            </div>
+            <div class="field">
+              <label class="label">Author:</label>
+              <div class="control has-icons-left">
+                <input id="form_author" class="input is-primary" type="text" name="form_author" value="<?php echo $_SESSION["read_one_entry"]["author"]; ?>" placeholder="Author of Post" required>
+                <span class="icon is-small is-left">
+                  <i class="material-icons prefix">account_circle</i>
+                </span>
+              </div>
+            </div>
+            <button class="button is-info" type="submit" value="<?php echo $_SESSION["read_one_entry"]["id"]?>" name="submit_update_request">
+              <span>Update</span>
+              <span class="icon">
+                <i class="material-icons prefix">send</i>
+              </span>
+            </button>
+          <?php endif; ?>
+          </form>
+        </div>
+
+        <!-- Remove the session read one array and reset the flag to false-->
+        <?php
+        unset($_SESSION["read_one_entry"]);
+        $_SESSION["read_one_entry_flag"] = false;
+        ?>
 
 
-  <!-- Scripts -->
-  <!-- <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script> -->
+        <!-- Scripts -->
+        <!-- <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script> -->
 </body>
 
 </html>
